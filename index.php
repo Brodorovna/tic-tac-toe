9d61d9bcc41f277c9692bb5a040c1040e010d8ac
Brodorovna <75303309+Brodorovna@users.noreply.github.com>
2021-03-07 18:37:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,74 +1,143 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+include 'functions.php';
 ?>
 
 <link rel="stylesheet" href="style.css">
 
 <?php
 
-//Получили данные из ж-сона, декодировали в массив и записали в $entries
-$content = file_get_contents('tictactoe_db.json');
-$entries = json_decode($content, true);
-
-//Проверяем, существует ли массив $entries. Если нет, то создаём.
-if (!is_array($entries)) {
-    $entries = [];
+if (array_key_exists('reset', $_REQUEST)) {
+    resetEntries();
 }
 
-// $entries['table'] = (array_key_exists('table', $entries) ? $entries['table'] : []); -- предложенный вариант с тернарным оператором
+$entries = getEntries();
+$table = &getTable($entries);
 
-//if classic для осознания происходящего и чтобы подключить созздание table, если его не было в jsone
-if (array_key_exists('table', $entries)) {
-    $entries['table'] = $entries['table'];
-} else {
-    $entries = ['table' => $entries]; //Если такого ключа не было, то он создаётся и ему присваивается массив $entries. 
-}
-
-//Переменной $table присваивается значение ключа table в масссиве $entries. 
-$table = &$entries['table']; // Вы можете передать переменную в функцию по ссылке, чтобы она могла изменять значение аргумента.
-
-//Если в массиве существуют ключи "строка" и "ячейка", то
 if (
-    array_key_exists('r', $_REQUEST) &&
-    array_key_exists('c', $_REQUEST)
+    array_key_exists('r', $_REQUEST) && array_key_exists('c', $_REQUEST)
 ) {
-    //Вывести в заголовке 3 значения этих ключей и привоить их значения переменным (чтобы было покороче) 
-    // echo "r=" . $_REQUEST['r'] . "; c= " . $_REQUEST['c'] . ' ';
     $a = $_REQUEST['r'];
     $b = $_REQUEST['c'];
 
-    //Если НЕ существует ключа count в массиве $entries, то он создаётся и ему присваивается значение 1
-    if (!array_key_exists('count', $entries)) {
-        $entries['count'] = 1;
-        // Если существует, то его текущее значение увеличивается на 1.
+    if (@$table[$a][$b] == 'x' || @$table[$a][$b] == 'O') {
+        echo '<h4>Field is reserved</h4>';
     } else {
-        $entries['count']++;
+        (!array_key_exists('count', $entries)) ? $entries['count'] = 1 : $entries['count']++;
+        ($entries['count'] % 2 != 0) ?   $table[$a][$b] = 'x' : $table[$a][$b] = 'O';
     }
 
-    // Если в активном поле есть х или о, то выводится сообщение, что поле занято
-    if ($table[$a][$b] == 'x' || $table[$a][$b] == 'O') {
-        echo '<h4>Field is reserved</h4>';
-        // в противном случае добавляется соотв. символ
-    } else {
-        // Если значение ключа count чётное, то в клетке ставится О, если нечётное, то Х.
-        if ($entries['count'] % 2 != 0) {
-            $table[$a][$b] = 'x';
-        } else {
-            $table[$a][$b] = 'O';
-        }
-    }
-    // Массив $entries шифруется и отправляется в json с применением эстетичного форматирования. 
-    file_put_contents('tictactoe_db.json', json_encode($entries, JSON_PRETTY_PRINT));
+    saveEntries($entries); // функция сохранения в в файл
 }
 
 ?>
 
 <div class="container">
-    <?php for ($r = 0; $r < 3; $r++) : ?>
-        <?php for ($c = 0; $c < 3; $c++) : ?>
-            <a href="?r=<?= $r ?>&c=<?= $c ?>"><?= $table[$r][$c]; ?></a>
-        <?php endfor; ?>
-    <?php endfor; ?>
+    <?php for ($r = 0; $r < 3; $r++) {
+        for ($c = 0; $c < 3; $c++) {
+            $value = (array_key_exists($r, $table) &&
+                array_key_exists($c, $table[$r]))
+                ? $table[$r][$c] : '';
+            echo "<a href='?r=$r&c=$c'>" . $value . "</a>";
+        }
+    }
+    ?>
+    <a href="?reset"> Reset </a>
 </div>
+
+
+<?php
+$row = $table[0];
+$row1 = $table[1];
+$row2 = $table[2];
+
+
+////////////////row
+switch (true) {
+    case ($row[0] == "x" && $row[1] == "x" && $row[2] == "x"):
+        echo "x won <br>";
+        break;
+    case ($row[0] == "O" && $row[1] == "O" && $row[2] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+switch (true) {
+    case ($row1[0] == "x" && $row1[1] == "x" && $row1[2] == "x"):
+        echo "x won <br>";
+        break;
+    case ($row1[0] == "O" && $row1[1] == "O" && $row1[2] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+switch (true) {
+    case ($row2[0] == "x" && $row2[1] == "x" && $row2[2] == "x"):
+        echo "x won <br>";
+        break;
+    case ($row2[0] == "O" && $row2[1] == "O" && $row2[2] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+//////////////////col
+switch (true) {
+    case ($table[0][0] == "x" && $table[1][0] == "x" && $table[2][0] == "x"):
+        echo "x won <br>";
+        break;
+    case ($table[0][0] == "O" && $table[1][0] == "O" && $table[2][0] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+switch (true) {
+    case ($table[0][1] == "x" && $table[1][1] == "x" && $table[2][1] == "x"):
+        echo "x won <br>";
+        break;
+    case ($table[0][1] == "O" && $table[1][1] == "O" && $table[2][1] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+
+switch (true) {
+    case ($table[0][2] == "x" && $table[1][2] == "x" && $table[2][2] == "x"):
+        echo "x won <br>";
+        break;
+    case ($table[0][2] == "O" && $table[1][2] == "O" && $table[2][2] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+/////////////cross
+switch (true) {
+    case ($table[0][0] == "x" && $table[1][1] == "x" && $table[2][2] == "x"):
+        echo "x won <br>";
+        break;
+    case ($table[0][0] == "O" && $table[1][1] == "O" && $table[2][2] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+switch (true) {
+    case ($table[0][0] == "x" && $table[1][1] == "x" && $table[2][2] == "x"):
+        echo "x won <br>";
+        break;
+    case ($table[0][0] == "O" && $table[1][1] == "O" && $table[2][2] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+switch (true) {
+    case ($table[0][2] == "x" && $table[1][1] == "x" && $table[2][0] == "x"):
+        echo "x won <br>";
+        break;
+    case ($table[0][2] == "O" && $table[1][1] == "O" && $table[2][0] == "O"):
+        echo "O won <br>";
+        break;
+}
+
+// ПРОВЕРКА ПОБЕДИТЕЛЯ ЧЕРЕЗ SWITCH (СРАВНИТЬ прямые и дагонали.)
